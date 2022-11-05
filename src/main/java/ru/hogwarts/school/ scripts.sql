d60faa8f580a0bacdf39a1258b5aea5d1896a11c
alikeli <eli-com2@yandex.ru>
2022-11-05 20:15:01 +0300

    wh 3.4

---
+++ src/main/java/ru/hogwarts/school/ scripts.sql
@@ -0,0 +1,6 @@
+select * from student;
+select * from student where age > 10 AND age < 20;
+select name from student ;
+select * from student where name like '%о%';
+select * from student where age < id;
+select * from student order by age;
